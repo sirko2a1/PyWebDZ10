402b7b38ade09c283cc835f85a9ce840d5ec1106
sirko2a1 <oleks.stri@gmail.com>
2025-06-21 18:14:10 +0300

    commit №4

--- mysite/quotes/views.py
+++ mysite/quotes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def start_page(request):
-    return render(request, 'quotes/start_page.html')
+    return render(request, 'start_page.html')
